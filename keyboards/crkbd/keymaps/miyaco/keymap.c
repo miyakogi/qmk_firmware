7d43d0fddab31556b9d0d2c688cbdd23f31b34f6
miyaco <miyako.dev@gmail.com>
2024-04-01 07:12:07 +0900

    Glow on bese layer

--- keyboards/crkbd/keymaps/miyaco/keymap.c
+++ keyboards/crkbd/keymaps/miyaco/keymap.c
@@ -149,7 +149,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch(layer) {
         case 0:
-            rgblight_sethsv(HSV_OFF);
+            // rgblight_sethsv(HSV_OFF);
+            rgblight_sethsv(HSV_GOLDENROD);
             break;
         case 6:
         case 7:
