6d50b62329debb549986b17623b23c41c6a34853
miyaco <miyako.dev@gmail.com>
2025-02-03 13:13:54 +0900

    [crkbd] remove some combo

--- keyboards/crkbd/keymaps/miyaco/keymap.c
+++ keyboards/crkbd/keymaps/miyaco/keymap.c
@@ -125,23 +125,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 enum combos {
     HENKAN_COMBO,
     MUHENKAN_COMBO,
-    ESC_COMBO,
-    SEMICOLON_COMBO,
-    COLON_COMBO,
 };
 
 const uint16_t PROGMEM henkan_combo[] = { RCTL_T(KC_N), RALT_T(KC_S), COMBO_END };
 const uint16_t PROGMEM muhenkan_combo[] = { LCTL_T(KC_A), LALT_T(KC_I), COMBO_END };
-const uint16_t PROGMEM esc_combo[] = { RSFT_T(KC_T), RCTL_T(KC_N), COMBO_END };
-const uint16_t PROGMEM semicolon_combo[] = { LSFT_T(KC_O), LCTL_T(KC_A), COMBO_END };
-const uint16_t PROGMEM colon_combo[] = { LALT_T(KC_I), LGUI_T(KC_E), COMBO_END };
 
 combo_t key_combos[] = {
     [HENKAN_COMBO] = COMBO(henkan_combo, JP_HENK),
     [MUHENKAN_COMBO] = COMBO(muhenkan_combo, JP_MHEN),
-    [ESC_COMBO] = COMBO(esc_combo, KC_ESC),
-    [SEMICOLON_COMBO] = COMBO(semicolon_combo, KC_SCLN),
-    [COLON_COMBO] = COMBO(colon_combo, KC_COLN),
 };
 
 bool get_combo_must_tap(uint16_t index, combo_t *combo) {
