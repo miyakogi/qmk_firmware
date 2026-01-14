de80790e7f95fbe09859d7af8e891f9e1c6abf4b
miyaco <miyako.dev@gmail.com>
2026-01-15 06:31:37 +0900

    [crkbd] update miyaco keymap

--- keyboards/crkbd/keymaps/miyaco/keymap.c
+++ keyboards/crkbd/keymaps/miyaco/keymap.c
@@ -61,11 +61,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_split_3x6_3(  // Function
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      RGB_HUI,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,                         KC_R,    KC_E,    KC_I,    KC_S,    KC_U,    KC_B,
+      XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,                         KC_R,    KC_E,    KC_I,    KC_S,    KC_U,    KC_B,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_VAI,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
+      XXXXXXX,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_SAI,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             TG(6), _______,  KC_APP,     KC_ENT, XXXXXXX, _______
                                       //`--------------------------'  `--------------------------'
@@ -83,19 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-    [5] = LAYOUT_split_3x6_3(  // Mouse (vim-like keymap) + Button
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                      XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y),                      XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    KC_BTN2, KC_BTN1, KC_BTN3
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-    [6] = LAYOUT_split_3x6_3(  // Gaming
+    [5] = LAYOUT_split_3x6_3(  // Gaming
   //,-----------------------------------------------------.                    ,----------------------------------------------------------------------------.
       XXXXXXX,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_Y,         KC_U,         KC_I,         KC_O,         KC_SCLN, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+-------------+-------------+-------------+----------------+--------|
@@ -103,12 +91,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+-------------+-------------+-------------+----------------+--------|
       XXXXXXX, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_N,         KC_M,      KC_COMM,       KC_DOT,         KC_SLSH, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+-------------+-------------+-------------+----------------+--------|
-                                           KC_ESC,   MO(7),  KC_SPC,     KC_ENT, KC_BSPC,        TG(6)
+                                           KC_ESC,   MO(6),  KC_SPC,     KC_ENT, KC_BSPC,        TG(5)
                                       //`--------------------------'  `-------------------------------'
 
   ),
 
-    [7] = LAYOUT_split_3x6_3(  // Gaming Number Layer
+    [6] = LAYOUT_split_3x6_3(  // Gaming Number Layer
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,  KC_TAB,    KC_7,    KC_8,    KC_9,    KC_T,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
