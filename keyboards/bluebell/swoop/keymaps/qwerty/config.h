b695d0f08972043d3f7f8b3932b94e469e04fdc4
miyaco <miyako.dev@gmail.com>
2026-01-15 06:32:10 +0900

    [swoop] add qwerty keymap

---
+++ keyboards/bluebell/swoop/keymaps/qwerty/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MASTER_LEFT
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define TAPPING_TERM 120
+#define TAPPING_TERM_PER_KEY
+
+#define COMBO_TERM 80
+#define COMBO_MUST_TAP_PER_COMBO
