5e8dc1af68acd63acb790681de35e03fa03f037b
miyaco <miyako.dev@gmail.com>
2026-01-15 06:32:34 +0900

    [crkbd] add qwerty keymap

---
+++ keyboards/crkbd/keymaps/qwerty/keymap.c
@@ -0,0 +1,181 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include QMK_KEYBOARD_H
+#include "keymap_japanese.h"
+
+enum layers {
+  _BASE,
+  _NUM,
+  _SYM,
+  _FUN,
+  _NAV,
+  _GAME,
+  _GAME_NUM,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_split_3x6_3(  // Default (大西配列mod)
+  //,--------------------------------------------------------------------------------.                                      ,------------------------------------------------------------------------------------------.
+       KC_TAB,         KC_Q,         KC_W,         KC_E,         KC_R,           KC_T,                                                     KC_Y,             KC_U,         KC_I,         KC_O,            KC_P, KC_MINS,
+  //|--------+-------------+-------------+-------------+-------------+---------------|                                      |------------------+-----------------+-------------+-------------+----------------+--------|
+      KC_LCTL, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F),           KC_G,                                                     KC_H,     RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+  //|--------+-------------+-------------+-------------+-------------+---------------|                                      |------------------+-----------------+-------------+-------------+----------------+--------|
+      KC_LSFT,         KC_Z,         KC_X,         KC_C,         KC_V,           KC_B,                                                     KC_N,             KC_M,      KC_COMM,       KC_DOT,         KC_SLSH, KC_BSLS,
+  //|--------+-------------+-------------+-------------+-------------+---------------+-----------------|  |-----------------+------------------+-----------------+-------------+-------------+----------------+--------|
+                                                               KC_ESC, LSFT_T(KC_SPC), LT(_NAV, KC_TAB),    LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), LT(_FUN, KC_DEL)
+                                                     //`-----------------------------------------------'  `------------------------------------------------------'
+
+  ),
+
+    [_NUM] = LAYOUT_split_3x6_3(  // Num 1 (left hand)
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_MINS,    KC_4,    KC_5,    KC_6,  KC_EQL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           KC_DOT,    KC_0, KC_QUOT,     KC_ENT, _______,  KC_ESC
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [_SYM] = LAYOUT_split_3x6_3(  // Symbols (with shift)
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_UNDS,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LPRN, KC_RPRN, KC_DQUO,    _______, XXXXXXX,  KC_ESC
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+    [_FUN] = LAYOUT_split_3x6_3(  // Function
+  //,-------------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX,  KC_F12,   KC_F7,   KC_F8,     KC_F9, KC_PSCR,                         KC_R,    KC_E,    KC_I,    KC_S,    KC_U,    KC_B,
+  //|--------+--------+--------+--------+----------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,  KC_F11,   KC_F4,   KC_F5,     KC_F6, KC_SCRL,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
+  //|--------+--------+--------+--------+----------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX,  KC_F10,   KC_F1,   KC_F2,     KC_F3, KC_PAUS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+----------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          TG(_GAME), _______,  KC_APP,     KC_ENT, XXXXXXX, _______
+                                      //`----------------------------'  `--------------------------'
+  ),
+
+    [_NAV] = LAYOUT_split_3x6_3(  // Nav (vim-like keymap) + Button
+  //,-----------------------------------------------------.                    ,-------------------------------------------------------.
+      XXXXXXX, C(KC_A), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+----------+--------+--------+--------+--------|
+      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                      KC_LEFT,   KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+----------+--------+--------+--------+--------|
+      XXXXXXX, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_Y),                      KC_HOME,   KC_PGDN, KC_PGUP,  KC_END,  KC_DEL,  KC_INS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+----------+--------+--------+--------+--------|
+                                          XXXXXXX, XXXXXXX, _______,     KC_ENT, XXXXXXX, TG(_GAME)
+                                      //`--------------------------'  `----------------------------'
+  ),
+
+    [_GAME] = LAYOUT_split_3x6_3(  // Gaming
+  //,-----------------------------------------------------.                          ,----------------------------------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,         KC_U,         KC_I,         KC_O,            KC_P, KC_MINS,
+  //|--------+--------+--------+--------+--------+--------|                          |--------+-------------+-------------+-------------+----------------+--------|
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                          |--------+-------------+-------------+-------------+----------------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,         KC_M,      KC_COMM,       KC_DOT,         KC_SLSH, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------------|  |--------+--------+-------------+-------------+-------------+----------------+--------|
+                                           KC_ESC,  KC_SPC, MO(_GAME_NUM),     KC_ENT, KC_BSPC,    TG(_GAME)
+                                      //`--------------------------------'  `-------------------------------'
+
+  ),
+
+    [_GAME_NUM] = LAYOUT_split_3x6_3(  // Gaming Number Layer
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_7,    KC_8,    KC_9,    KC_T,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_A,    KC_4,    KC_5,    KC_6,    KC_G,                      XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_1,    KC_2,    KC_3,    KC_B,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______, _______, _______,     KC_ENT, KC_BSPC,  KC_ESC
+                                      //`--------------------------'  `--------------------------'
+
+  )
+};
+
+enum combos {
+    HENKAN_COMBO,
+    MUHENKAN_COMBO,
+};
+
+const uint16_t PROGMEM henkan_combo[] = { RCTL_T(KC_K), RALT_T(KC_L), COMBO_END };
+const uint16_t PROGMEM muhenkan_combo[] = { LCTL_T(KC_D), LALT_T(KC_S), COMBO_END };
+
+combo_t key_combos[] = {
+    [HENKAN_COMBO] = COMBO(henkan_combo, JP_HENK),
+    [MUHENKAN_COMBO] = COMBO(muhenkan_combo, JP_MHEN),
+};
+
+bool get_combo_must_tap(uint16_t index, combo_t *combo) {
+    return true;
+}
+
+
+#ifdef TAPPING_TERM_PER_KEY
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case LGUI_T(KC_A):
+        case RGUI_T(KC_SCLN):
+            return TAPPING_TERM + 100;
+        case LALT_T(KC_S):
+        case RALT_T(KC_L):
+            return TAPPING_TERM + 120;
+        case LCTL_T(KC_D):
+        case RCTL_T(KC_K):
+            return TAPPING_TERM + 70;
+        case LSFT_T(KC_F):
+        case RSFT_T(KC_J):
+            return TAPPING_TERM + 20;
+        default:  // thumb keys
+            return TAPPING_TERM;
+    }
+}
+#endif
+
+
+// RGB Layer
+// see: https://note.com/yinouet1001/n/na3b132c060f3
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton32(state);
+    switch(layer) {
+        case _BASE:
+            // rgblight_sethsv(HSV_OFF);
+            rgblight_sethsv(HSV_GOLDENROD);
+            break;
+        case _GAME:
+        case _GAME_NUM:
+            rgblight_sethsv(HSV_PURPLE);
+            break;
+        default:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+    }
+
+    return state;
+}
