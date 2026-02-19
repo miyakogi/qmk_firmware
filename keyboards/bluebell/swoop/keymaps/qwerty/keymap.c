35cad93da0eea5768b775e104387b4371b9b44ca
miyaco <miyako.dev@gmail.com>
2026-02-19 09:11:49 +0900

    [swoop] change layer color

--- keyboards/bluebell/swoop/keymaps/qwerty/keymap.c
+++ keyboards/bluebell/swoop/keymaps/qwerty/keymap.c
@@ -165,7 +165,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_sethsv(HSV_PURPLE);
             break;
         default:
-            rgblight_sethsv(HSV_PINK);
+            rgblight_sethsv(HSV_TURQUOISE);
             break;
     }
 
